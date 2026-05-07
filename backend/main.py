ac135c166834eaa3780e2f57f47dcbba6355c9ec
Sohan <avengers6452038@gmail.com>
2026-05-07 19:06:02 +0530

    Updated backend for 6 features

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,10 @@ model = joblib.load("model.pkl")
 class StudentData(BaseModel):
     study_hours: float
     attendance: float
+    quiz_score: float
+    assignment_score: float
+    midterm_score: float
+    projects_completed: float
 
 @app.get("/")
 def home():
@@ -27,8 +31,16 @@ def home():
 @app.post("/predict")
 def predict(data: StudentData):
     prediction = model.predict(
-        np.array([[data.study_hours, data.attendance]])
-    )
+           np.array([[
+        data.study_hours,
+        data.attendance,
+        data.quiz_score,
+        data.assignment_score,
+        data.midterm_score,
+        data.projects_completed
+    ]])
+)
+
 
     return {
        "predicted_score": round(float(prediction[0]), 2)
